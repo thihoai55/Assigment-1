ef800a802c6c920d336abe67110c5f130b9c0182
Nguyen Thi Hoai <nguyenthihoai552004@gmail.com>
2025-06-29 13:54:28 +0700

    update thêm nút thông báo xin chào

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,9 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myproject.ui.theme.MyProjectTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingWithButton()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingWithButton() {
+    val context = LocalContext.current
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Button(onClick = {
+                Toast.makeText(context, "xin chào", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Nhấn vào đây")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyProjectTheme {
-        Greeting("Android")
+        GreetingWithButton()
     }
 }
